8d07b1a35ab426c3e1aeb8dc9c4b4974e510104c
fabricioFalcao <fabriciohfd@gmail.com>
2023-10-04 20:59:47 -0300

    Requisito 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,20 @@
+import { useSelector } from 'react-redux';
+import { GlobalStateType } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const { user: { email } } = useSelector((state: GlobalStateType) => state);
+
+  return (
+    <header>
+      <p data-testid="email-field">{email}</p>
+      <p>
+        Despesa total:
+        {' '}
+        <span data-testid="total-field">0</span>
+        {' '}
+        <span data-testid="header-currency-field">BRL</span>
+      </p>
+    </header>
+  );
 }
 export default Header;
